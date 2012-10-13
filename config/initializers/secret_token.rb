51f21c127c62a6b0c16194145a6c32468445c6b2
Dan Hackner <dan.hackner@gmail.com>
2012-10-13 13:09:41 -0700

    Push default rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalWebsite::Application.config.secret_token = 'd238d823df6d86c73004b80375983e2d1f5dc83201b5b58d868c3e5f9ffd7db2bec8a5768ad51b70a0a91119d841ff45ff212cf85c5ecb2b05e4fa2a8264ff35'
