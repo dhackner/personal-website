2277123fea47cc62af93953e58496865041561e9
Dan Hackner <dan.hackner@gmail.com>
2012-10-14 23:39:19 -0700

    - Added SUSY and sassy-buttons dependencies - Generated static scaffolding - Right gutter layout - Angellist badge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PersonalWebsite::Application.routes.draw do
+  root :to => 'static#homepage'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ PersonalWebsite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'public#index'
 
   # See how all your routes lay out with "rake routes"
 
